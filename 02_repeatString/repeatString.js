d77d6c8e8e87921ef085e4dc12b587a686e6d651
David Ferguson <dvf@gmx.com>
2023-03-26 14:25:37 +0100

    fix syntax error at for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
 const repeatString = function(word, times) {
     let string = "";
-    for )let i = 0; i < times; i++) {
+    for (let i = 0; i < times; i++) {
         string += word;
     }
     return string
