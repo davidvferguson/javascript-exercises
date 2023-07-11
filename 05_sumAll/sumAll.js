9196c183399f9057aaf3669320d31694fa8a8a5b
David Ferguson <dvf@gmx.com>
2023-07-11 21:18:29 +0100

    05 sumAll first attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+let total = 
+    do {
+    a += a;
+} while (a < b);
 };
+
+return total
 
 // Do not edit below this line
 module.exports = sumAll;
