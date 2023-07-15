1bff09b74b2ec058b8d80ec9709e3a9ebfc0f276
David Ferguson <dvf@gmx.com>
2023-07-15 20:59:18 +0100

    05_sumAll all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,30 @@
 const sumAll = function(a, b) {
 
-let total = 
-    for ( i = a; i < b; i++ )
-    a += a;
+    let i = a;
+
+    if (a < 0 || typeof a != "number" || typeof b != "number") {
+        return "ERROR"
+    };
+
+    if (a > b) {
+        while (i > b){
+            i--;
+            a += i;
+        }
+    
+    }
+    
+    while (i < b){
+        i++;
+        a += i;
+    }
+    
+    let total = a;
+    return total;
 
 };
 
-return total
+
 
 // Do not edit below this line
 module.exports = sumAll;
