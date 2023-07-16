88803b12b2098466d4d78219d3da435c384905d2
David Ferguson <dvf@gmx.com>
2023-07-16 20:38:57 +0100

    07_tempConversion all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * (5 / 9);
+  let ans = Math.round(c * 10) / 10;
+  return ans;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9 / 5) + 32;
+  let ans = Math.round(f * 10) / 10;
+  return ans;
 };
 
 // Do not edit below this line
